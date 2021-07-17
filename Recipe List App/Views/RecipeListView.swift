465b6223aaad9a19ca512df956c767bf65a87a36
Lea <lea.dukaez@gmail.com>
2021-07-17 13:05:34 +0200

    Change List to Scroll View LazyVStack forEach

--- Recipe List App/Views/RecipeListView.swift
+++ Recipe List App/Views/RecipeListView.swift
@@ -14,31 +14,47 @@ struct RecipeListView: View {
     var body: some View {
         
         NavigationView {
-            List(model.recipes) { r in
+            
+            VStack(alignment: .leading) {
+                Text("All Recipes")
+                    .bold()
+                    .padding(.top, 40)
+                    .font(.largeTitle)
                 
-                NavigationLink(
-                    destination: RecipeDetailView(recipe: r),
-                    label: {
-                        
-                        // MARK: Row Item
-                        HStack(spacing: 20.0) {
-                            Image(r.image)
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 60.0, height: 60.0)
-                                .clipped()
-                                .cornerRadius(5)
-                            Text(r.name)
+                ScrollView {
+                    LazyVStack(alignment: .leading, spacing: 10) {
+                        ForEach(model.recipes) { r in
+                            
+                            NavigationLink(
+                                destination: RecipeDetailView(recipe: r),
+                                label: {
+                                    
+                                    // MARK: Row Item
+                                    HStack(spacing: 20.0) {
+                                        Image(r.image)
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 60.0, height: 60.0)
+                                            .clipped()
+                                            .cornerRadius(5)
+                                        Text(r.name).foregroundColor(.black)
+                                    }
+                                })
                         }
-                    })
+                    }
+                    
+                }
                 
-            }.navigationBarTitle("All Recipes")
+                
+            }.navigationBarHidden(true)
+            .padding(.leading)
+            
         }
     }
 }
 
 struct RecipeListView_Previews: PreviewProvider {
     static var previews: some View {
-        RecipeListView()
+        RecipeListView().environmentObject(RecipeModel())
     }
 }
